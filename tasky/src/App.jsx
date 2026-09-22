a7760be88764fede5e5c4577cdf8a2038c486f71
joey0 <20112816@mail.wit.ie>
2026-09-22 12:12:19 +0100

    priorities

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -5,21 +5,23 @@ import React, { useState } from 'react';
 
 function App() {
   const [ taskState, setTaskState ] = useState({
-    task: [
-      { id: 1, title: "Dishes", description: "Empty dishwashers", deadline: "Today"},
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow"},
-      { id: 3, title: "Tidy up", deadline: "Today" }
+    tasks: [
+      { id: 1, title: "Dishes", description: "Empty dishwashers", deadline: "Today", priority: "Low"},
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "Medium"},
+      { id: 3, title: "Tidy up", deadline: "Today", priority: "High"}
     ]
 });
 
   return (
     <div className="Container">
       <h1>Tasky</h1>
+
       {taskState.tasks.map((task) => (
       <Task
         title={task.title}
         description={task.description}
         deadline={task.deadline}
+        priority={task.priority}
         />
       ))}
     </div>
