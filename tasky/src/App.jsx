39825cf4aa69f88415935efa9abd5470eef820cc
joey0 <20112816@mail.wit.ie>
2026-09-24 09:20:06 +0100

    form

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
+import AddTaskForm from './components/Form';
 
 
 function App() {
@@ -42,6 +43,7 @@ const deleteHandler = (taskIndex) => {
         
         />
       ))}
+      <AddTaskForm />
     </div>
   );
 }
